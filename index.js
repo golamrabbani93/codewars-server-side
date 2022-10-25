325d0d4b526525aa13d3798d16306d810abf6eff
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-25 12:48:16 +0600

    send courses data to server

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 //*send all courses Category
 app.get('/course-category', (req, res) => {
 	res.send(categories);
 });
-
+//* send all Courses
+app.get('/courses', (req, res) => {
+	res.send(courses);
+});
 app.get('/', (req, res) => {
 	res.send('Code war server is runing');
 });
