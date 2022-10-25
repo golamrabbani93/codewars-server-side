ee35e6f4e3b860c57fffc228e9cf0e3de4530421
golamrabbani93 <golamrabbani9373@gmail.com>
2022-10-25 12:44:48 +0600

    create all courses fakedate

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const categories = require('./data/categories.json');
+
+//*send all courses Category
+app.get('/course-category', (req, res) => {
+	res.send(categories);
+});
+
 app.get('/', (req, res) => {
 	res.send('Code war server is runing');
 });
